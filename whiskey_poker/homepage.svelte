ae7f8f4d50bc0664b02706f6135e0446db5640a1
MikeGordon <vprmatrix55@gmail.com>
2025-08-11 15:29:33 -0700

    added svelte

---
+++ whiskey_poker/homepage.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    let name = "Cask & Co."
+</script>
+
+<h1>Welcom to {name}</h1>
+
+<p>This website will be used to update members of Cask & Co on upcoming events, collect dues,
+    display a leader board, and vote on the next bottle.
+</p>
+<style>
+    p {
+        color: white;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 2em;
+    }
+</style>
